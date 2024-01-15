e6798d2b9aa5d9a053d4217138fd9b509d19f9c6
Timafey Klyuykov <tima.yoer@gmail.com>
2024-01-16 00:41:07 +0500

    super в цепочке наследования

--- src/BMW.java
+++ src/BMW.java
@@ -1,7 +1,17 @@
 public class BMW extends Car {
 
+    public BMW(String name, String model, String color) {
+        super(name, model, color);
+    }
+
+    public BMW() {
+    }
+
     @Override
     public void drive(){
+
+        super.signal();
+        super.drive();
         System.out.println("drive BMW");
     }
 }
